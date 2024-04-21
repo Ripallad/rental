b4518c5fd3e3b50a64236ba1e84c763f519a4a71
Ripallad <ripallad123@gmail.com>
2024-04-21 10:27:29 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rental/firebase_options.dart';
+import 'package:rental/main_screens/navigation.dart';
 
 import 'package:rental/main_screens/splash.dart';
 
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Splashscreen(),
+        home: navigation(),
       ),
     );
   }
